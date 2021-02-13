e7e689069884a031e88c16d7b42cd1b9a7c3a153
Camila Matos <camila.mila148@gmail.com>
2021-02-13 17:28:07 -0300

    definido os movimentos da cobrinha

--- script.js
+++ script.js
@@ -8,6 +8,8 @@ snake[0] = {
     y: 8 * box
 }
 
+let direction = 'right';
+
 function criarBG(){
     context.fillStyle = "lightgreen";
     context.fillRect(0,0,16 * box, 16 * box);
@@ -20,5 +22,29 @@ function criarSnake(){
     }
 }
 
-criarBG();
-criarSnake();
+function iniciarJogo(){
+    criarBG();
+    criarSnake();
+
+    // posição inicial
+    let snakeX = snake[0].x;
+    let snakeY = snake[0].y;
+
+    //cordenadas
+    if(direction == 'right'){ snakeX += box;}
+    if(direction == 'left'){ snakeX -= box;}
+    if(direction == 'up'){ snakeY -= box;}
+    if(direction == 'down'){ snakeY += box;}
+
+    snake.pop(); // retira o ultimo elemento do array
+
+    //criando uma nova cabeça a frente
+    let newHead = {
+        x: snakeX,
+        y: snakeY
+    }
+
+    snake.unshift(newHead); //adicina um novo elemente a frente
+}
+
+let jogo = setInterval(iniciarJogo,100);
